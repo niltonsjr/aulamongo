4772ab0265fd449363f5b4b06f38f934a9fe6de0
Nilton da Silva Junior <tonsti@gmail.com>
2021-03-16 20:42:00 +0100

    Project Created

---
+++ src/main/java/com/devsuperior/aulamongo/AulamongoApplication.java
@@ -0,0 +1,13 @@
+package com.devsuperior.aulamongo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AulamongoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AulamongoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/devsuperior/aulamongo/AulamongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior.aulamongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AulamongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

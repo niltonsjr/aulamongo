a1535c9e4789b92e407dbb954a8b35a7cdbad507
Nilton da Silva Junior <tonsti@gmail.com>
2021-03-17 21:12:42 +0100

    Put user

--- src/main/java/com/devsuperior/aulamongo/controllers/UserController.java
+++ src/main/java/com/devsuperior/aulamongo/controllers/UserController.java
@@ -8,6 +8,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,26 +20,33 @@ import com.devsuperior.aulamongo.services.UserService;
 @RestController
 @RequestMapping(value = "/users")
 public class UserController {
-	
+
 	@Autowired
 	private UserService service;
-	
+
 	@GetMapping
-	public ResponseEntity<List<UserDTO>> findAll(){
+	public ResponseEntity<List<UserDTO>> findAll() {
 		List<UserDTO> list = service.findAll();
 		return ResponseEntity.ok().body(list);
 	}
-	
+
 	@GetMapping(value = "/{id}")
 	public ResponseEntity<UserDTO> findById(@PathVariable String id) {
 		UserDTO obj = service.findById(id);
 		return ResponseEntity.ok().body(obj);
 	}
-	
+
 	@PostMapping
 	public ResponseEntity<UserDTO> insert(@RequestBody UserDTO obj) {
 		obj = service.insert(obj);
 		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
 		return ResponseEntity.created(uri).body(obj);
 	}
+
+	@PutMapping(value = "/{id}")
+	public ResponseEntity<UserDTO> update(@RequestBody UserDTO obj, @PathVariable String id) {
+		obj = service.update(id, obj);
+		return ResponseEntity.ok(obj);
+	}
+
 }
--- src/main/java/com/devsuperior/aulamongo/services/UserService.java
+++ src/main/java/com/devsuperior/aulamongo/services/UserService.java
@@ -14,7 +14,7 @@ import com.devsuperior.aulamongo.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class UserService {
-	
+
 	@Autowired
 	private UserRepository repository;
 
@@ -22,13 +22,12 @@ public class UserService {
 		List<User> list = repository.findAll();
 		return list.stream().map(x -> new UserDTO(x)).collect(Collectors.toList());
 	}
-	
+
 	public UserDTO findById(String id) {
-		Optional<User> result = repository.findById(id); 
-		User entity = result.orElseThrow(() -> new ResourceNotFoundException("Recurso no encontrado"));
+		User entity = getEntityById(id);
 		return new UserDTO(entity);
 	}
-	
+
 	public UserDTO insert(UserDTO dto) {
 		User entity = new User();
 		copyDtoToEntity(dto, entity);
@@ -36,8 +35,21 @@ public class UserService {
 		return new UserDTO(entity);
 	}
 
+	public UserDTO update(String id, UserDTO dto) {
+		User entity = getEntityById(id);
+		copyDtoToEntity(dto, entity);
+		entity = repository.save(entity);
+		return new UserDTO(entity);
+	}
+
+	private User getEntityById(String id) {
+		Optional<User> result = repository.findById(id);
+		return result.orElseThrow(() -> new ResourceNotFoundException("Recurso no encontrado"));
+	}
+
 	private void copyDtoToEntity(UserDTO dto, User entity) {
 		entity.setName(dto.getName());
-		entity.setEmail(dto.getEmail());		
+		entity.setEmail(dto.getEmail());
 	}
+
 }

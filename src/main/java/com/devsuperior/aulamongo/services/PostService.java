1c1da3f67945e6946660d277c12a63aeec7a6912
Nilton da Silva Junior <tonsti@gmail.com>
2021-03-25 22:10:11 +0100

    Get Post full search

--- src/main/java/com/devsuperior/aulamongo/services/PostService.java
+++ src/main/java/com/devsuperior/aulamongo/services/PostService.java
@@ -7,6 +7,8 @@ import com.devsuperior.aulamongo.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.time.Instant;
+import java.time.format.DateTimeParseException;
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
@@ -30,5 +32,20 @@ public class PostService {
 	public List<PostDTO> findByTitle(String text) {
 		List<Post> list = repository.searchTitle(text);
 		return list.stream().map(x -> new PostDTO(x)).collect(Collectors.toList());
+	}
+
+	public List<PostDTO> fullSearch(String text, String start, String end) {
+		Instant startMoment = convertMoment(start, Instant.ofEpochMilli(0L));
+		Instant endMoment = convertMoment(end, Instant.now());
+		List<Post> list = repository.fullSearch(text, startMoment, endMoment);
+		return list.stream().map(x -> new PostDTO(x)).collect(Collectors.toList());
+	}
+
+	private Instant convertMoment(String originalString, Instant alternative) {
+		try{
+			return Instant.parse(originalString);
+		} catch (DateTimeParseException e) {
+			return alternative;
+		}
 	}
 }

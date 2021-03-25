705dba93912667b8f3bb3a32697cc8b91f92d7e4
Nilton da Silva Junior <tonsti@gmail.com>
2021-03-25 21:42:37 +0100

    Get Post by Title, query operators

--- src/main/java/com/devsuperior/aulamongo/repositories/PostRepository.java
+++ src/main/java/com/devsuperior/aulamongo/repositories/PostRepository.java
@@ -2,12 +2,17 @@ package com.devsuperior.aulamongo.repositories;
 
 import com.devsuperior.aulamongo.models.entities.Post;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
 public interface PostRepository extends MongoRepository<Post, String>{
+
+    //https://docs.mongodb.com/manual/reference/operator/query/regex/
+    @Query("{ 'title': { $regex: ?0, $options: 'i' } }")
+    List<Post> searchTitle(String text);
 
     List<Post> findByTitleContainingIgnoreCase(String text);
 }
